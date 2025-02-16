12b068e07a9b7f5ee5a29a4227f8082e9a0380df
BPilecarte <beatrizpilecart3@gmail.com>
2025-02-15 21:55:27 -0300

    primeiro commit

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercicio5
+{
+    class Program
+    {
+        public static void CalcularMedia() 
+        {
+            Console.WriteLine("Digite a primeira nota");
+            decimal nota1 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota");
+            decimal nota2 = decimal.Parse(Console.ReadLine());
+
+            decimal media = (nota1 +  nota2) / 2;
+            Console.WriteLine($"A média é {media}");
+
+            if (media >= 7)
+                Console.WriteLine("Aprovado");
+            else if (media < 7 && media >= 4)
+                Console.WriteLine("Recuperação");
+            else
+                Console.WriteLine("Reprovado");
+        }
+    }
+}
